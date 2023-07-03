85ac2b9082be67784c2ca230903d10e8ed026adc
Ruslan Rabadanov <vonadabarr@gmail.com>
2023-07-03 16:15:09 +0200

    templates

--- products/views.py
+++ products/views.py
@@ -4,7 +4,40 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'products/index.html')
+    context = {
+        'title': 'Store',
+        'is_promotion': False,
+    }
+    return render(request, 'products/index.html', context)
 
 def products(request):
-    return render(request, 'products/products.html')
+    context = {
+        'products' : [
+            {
+                'name': 'Худи черного цвета с монограммами adidas Originals',
+                'price': 6_090,
+                'img': '/static/vendor/img/products/Adidas-hoodie.png',
+                'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.',
+            },
+            {
+                'name': 'Синяя куртка The North Face',
+                'price': 23_730,
+                'img': '/static/vendor/img/products/Blue-jacket-The-North-Face.png',
+                'description': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.',
+            },
+            {
+                'name': 'Коричневый спортивный oversized-топ ASOS DESIGN',
+                'price': 3_390,
+                'img': '/static/vendor/img/products/Brown-sports-oversized-top-ASOS-DESIGN.png',
+                'description': 'Мягкий трикотаж. Эластичный материал.',
+            },
+            {
+                'name': 'Черный рюкзак Nike Heritage',
+                'price': 2_340,
+                'img': '/static/vendor/img/products/Black-Nike-Heritage-backpack.png',
+                'description': 'Плотная ткань. Легкий и прочный материал.',
+            },
+        ]
+    }
+
+    return render(request, 'products/products.html', context)
